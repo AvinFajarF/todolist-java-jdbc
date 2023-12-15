37b6d15daa745d1f025139740178442ade3fef25
AvinFajarF <avinfajar6@gmail.com>
2023-12-15 23:36:18 +0700

    feat: Menambahkan entity TodoList untuk menyimpan daftar tugas

---
+++ src/main/java/project/todo/list/first/entity/TodoList.java
@@ -0,0 +1,45 @@
+package project.todo.list.first.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class TodoList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public TodoList(String todo) {
+        this.todo = todo;
+    }
+
+    @Column(name = "todo")
+    private String todo;
+
+    public TodoList() {
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTodo() {
+        return todo;
+    }
+
+    public void setTodo(String todo) {
+        this.todo = todo;
+    }
+
+    @Override
+    public String toString() {
+        return "TodoList{" +
+                "id=" + id +
+                ", todo='" + todo + '\'' +
+                '}';
+    }
+}
