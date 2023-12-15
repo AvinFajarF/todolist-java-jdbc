0338ba8aa41570dc99e20fc52880148f3f673ca2
AvinFajarF <avinfajar6@gmail.com>
2023-12-16 00:29:26 +0700

    feat: menambahkan TodoList service

--- src/main/java/project/todo/list/first/service/TodoListService.java
+++ src/main/java/project/todo/list/first/service/TodoListService.java
@@ -1,2 +1,26 @@
-package project.todo.list.first.service;public interface TodoListService {
+package project.todo.list.first.service;
+
+import project.todo.list.first.entity.TodoList;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TodoListService {
+
+//    TODO: mendapatkan semua todo
+//    TODO: menyimpan todo
+//    TODO: menampilkan detail todo
+//    TODO: update todo
+//    TODO: hapus todo
+
+    public List<TodoList> getTodoAll();
+
+    public void saveTodo(TodoList todoList);
+
+    public Optional<TodoList> getTodoById(Long id);
+
+    public void updateTodo(Long id, String todo);
+
+    public void deleteTodo(Long id);
+
 }
--- src/main/java/project/todo/list/first/service/TodoListServiceImpl.java
+++ src/main/java/project/todo/list/first/service/TodoListServiceImpl.java
@@ -1,2 +1,61 @@
-package project.todo.list.first.service;public class TodoListServiceImpl {
+package project.todo.list.first.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import project.todo.list.first.entity.TodoList;
+import project.todo.list.first.repository.TodoListRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TodoListServiceImpl implements TodoListService{
+//    TODO: implementasi mendapatkan semua todo
+//    TODO: implementasi menyimpan todo
+//    TODO: implementasi menampilkan detail todo
+//    TODO: implementasi update todo
+//    TODO: implementasi hapus todo
+
+    private final TodoListRepository todoListRepository;
+
+    @Autowired
+    public TodoListServiceImpl(TodoListRepository repository) {
+        this.todoListRepository = repository;
+    }
+
+    @Override
+    public List<TodoList> getTodoAll() {
+        return todoListRepository.findAll();
+    }
+
+    @Override
+    public void saveTodo(TodoList todoList) {
+    todoListRepository.save(todoList);
+    }
+
+    @Override
+    public Optional<TodoList> getTodoById(Long id) {
+        return todoListRepository.findById(id);
+    }
+
+    @Override
+    public void updateTodo(Long id, String todo) {
+        TodoList todoList = todoListRepository
+                .findById(id)
+                .orElseThrow(() -> new IllegalStateException("Todo list tidak di temukan"));
+//        check jika ada request todo
+        if (todo != null && !todo.isEmpty()){
+            todoList.setTodo(todo);
+        }
+    }
+
+    @Override
+    public void deleteTodo(Long id) {
+    Optional<TodoList> todoList = todoListRepository.findById(id);
+//    check data ada di database atau tidak
+        if (todoList.isPresent()){
+            todoListRepository.deleteById(id);
+        }
+    }
+
 }
